fa35d3fa5cbea66e4e65b14d984640d8e40c3ea8
Matt Shoemaker <shoemakerconsulting@gmail.com>
2020-12-01 16:27:05 -0800

    changed .vbs file path

--- run_script.bat
+++ run_script.bat
@@ -1,3 +1,2 @@
-#cd "c:\windows\users\matt\documents\vbs"
-cd "C:\Users\Matt\Google Drive\Business\Practical Research Inc\CDC\vbs"
+cd "C:\Users\Matt\Google Drive\git\refresh_tableau_public\"
 refresh_and_publish.vbs
